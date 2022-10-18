39eb9b945961b98b71e1bb33c5c5e5835883f6fd
Stanley <stanleytanyr@gmail.com>
2022-10-18 21:07:11 +0800

    created db and table

--- payment.php
+++ payment.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <title>SJ STORE</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="main">
         <div class="navbar">
@@ -18,13 +20,11 @@
                 <a href="">Cart</a>
             </div>
         </div>
-        <div id="aboutus">
-            <div id="aboutusleft">
-                <h2>About us</h2>
-                <img src="res/coollogo_com-63181092.png" alt="">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est suscipit at, consectetur vero nam temporibus deleniti. Odio, pariatur delectus voluptates adipisci provident deserunt non doloribus accusamus necessitatibus incidunt nesciunt, optio nobis iusto eum beatae quam voluptas fugiat repellendus sit. Excepturi aliquam neque ullam explicabo dignissimos, illum eaque blanditiis a consequatur.</p>
+        <div id="cartpage">
+            <div id="cartleft">
+                <?php include 'product_table.php'; ?>
             </div>
-            <div id="aboutusright">
+            <div id="cartright">
                 <h2>Contact us!</h2>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, blanditiis.</p>
                 <h5>Other useful Links</h5>
@@ -38,4 +38,5 @@
         </footer>
     </div>
 </body>
+
 </html>
