3d2c935e3df018ad45380806ea4dec3910331732
Stanley <stanleytanyr@gmail.com>
2022-11-03 09:38:59 +0800

    updated sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,21 +1,10 @@
 CREATE TABLE `users` 
-(
-    accountId int UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    username varchar(64) NOT NULL UNIQUE,
-    password varchar(64) NOT NULL
-);
-
-CREATE TABLE `CustomerDetails` 
-(
-    custId int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    accountId int UNSIGNED,
-    fullName varchar(128),
-    email varchar(128),
-    phoneNumber varchar(30),
-    address varchar(256),
-    dateOfBirth Date,
-    FOREIGN KEY (accountId)
-        REFERENCES Account(accountId)
+(   id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    privilege varchar(50) NOT NULL,
+    email int varchar(50), 
+    username varchar(50) NOT NULL UNIQUE,
+    password varchar(50) NOT NULL,
+    create_datetime DATETIME
 );
 
 CREATE TABLE `Product` 
@@ -29,41 +18,19 @@ CREATE TABLE `Product`
     size varchar(45),
     stock int,
     description TEXT,
-    image1 varchar(255),
+    image varchar(255),
     image2 varchar(255),
     image3 varchar(255),
     image4 varchar(255)
 );
 
-CREATE TABLE `Colors` 
-(
-    colorId int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    productId int UNSIGNED,
-    color_name varchar(45),
-    FOREIGN KEY (productId)
-        REFERENCES Product(productindex)
-);
-
-CREATE TABLE `Size` 
-(
-    sizeId int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    productId int UNSIGNED,
-    size varchar(45),
-    FOREIGN KEY (productId)
-        REFERENCES Product(productindex)
-);
-
-
-/* status = 0 for in cart, 1 if paid */
 CREATE TABLE `Order` 
 (
     orderId int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    status int, 
-    custId int UNSIGNED,
+    username varchar(50),
+    email varchar(100), 
     purchasedDate date,
     totalAmount float(16,2),
-    FOREIGN KEY (custId)
-        REFERENCES CustomerDetails(custId)
 );
 
 CREATE TABLE `OrderItems` 
@@ -74,8 +41,6 @@ CREATE TABLE `OrderItems`
     quantity int,
     FOREIGN KEY (orderId)
         REFERENCES `Order`(orderId),
-    FOREIGN KEY (productId)
-        REFERENCES Product(productindex)
 );
 
 
