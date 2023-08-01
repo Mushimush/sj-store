51db4b388ce671b2adf002c412c8400d9e95d763
Stanley <stanleytanyr@gmail.com>
2023-08-01 14:10:25 +0800

    Updated Readme

--- registration.php
+++ registration.php
@@ -38,7 +38,7 @@
         }
     } else {
     ?>
-        <img src="res/SJLOGO.JPG" alt="">
+        <img id="registrationlogo" src="res/SJLOGO.JPG" alt="" width="150">
         <form class="form" action="" method="post">
             <h1 class="login-title">Registration</h1>
             <div class="inputmax">
@@ -47,14 +47,14 @@
                     <input type="radio" id="user" class="login-input" name="privilege" value="user" required>
                 </div>
                 <div class="input">
-                    <label class="inputlabel" for="user">Admin</label>
+                    <label class="inputlabel" for="user ">Admin</label>
                     <input type="radio" id="admin" class="login-input" name="privilege" value="admin" required>
                 </div>
 
             </div>
 
             <input type="text" class="login-input" name="username" placeholder="Username" required />
-            <input type="text" class="login-input" name="email" placeholder="Email Adress">
+            <input type="email" class="login-input" name="email" placeholder="Email Adress">
             <input type="password" class="login-input" name="password" placeholder="Password">
             <input type="submit" name="submit" value="Register" class="login-button">
             <p class="link"><a href="login.php">Click to Login</a></p>
